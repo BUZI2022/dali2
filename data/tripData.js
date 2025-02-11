fe3bac1d8527f717007d96eb24bc1f81fa10e671
BUZI2022 <2200015884@stu.pku.edu.cn>
2025-02-11 13:45:37 +0800

    Update tripData.js

--- data/tripData.js
+++ data/tripData.js
@@ -1,69 +1,69 @@
 window.tripData = {
     photos: [
-        { id: 1, name: '洱海海鸥', thumbnail: 'pic/photos/洱海海鸥.jpg', fullImage: 'pic/photos/洱海海鸥.jpg' },
-        { id: 2, name: '洱海孤树', thumbnail: 'pic/photos/洱海孤树.jpg', fullImage: 'pic/photos/洱海孤树.jpg' },
-        { id: 3, name: '洱海月', thumbnail: 'pic/photos/洱海月.jpg', fullImage: 'pic/photos/洱海月.jpg' },
-        { id: 4, name: '龙湾俯瞰', thumbnail: 'pic/photos/龙湾俯瞰.jpg', fullImage: 'pic/photos/龙湾俯瞰.jpg' },
-        { id: 5, name: '海东', thumbnail: 'pic/photos/海东.jpg', fullImage: 'pic/photos/海东.jpg' },
-        { id: 6, name: '海东滩涂', thumbnail: 'pic/photos/海东滩涂.jpg', fullImage: 'pic/photos/海东滩涂.jpg' },
-        { id: 7, name: '孤舟', thumbnail: 'pic/photos/孤舟.jpg', fullImage: 'pic/photos/孤舟.jpg' },
-        { id: 8, name: '凭栏小队', thumbnail: 'pic/photos/凭栏小队.jpg', fullImage: 'pic/photos/凭栏小队.jpg' },
-        { id: 9, name: '感通索道集市', thumbnail: 'pic/photos/感通索道集市.jpg', fullImage: 'pic/photos/感通索道集市.jpg' },
-        { id: 10, name: '感通索道停车场', thumbnail: 'pic/photos/感通索道停车场.jpg', fullImage: 'pic/photos/感通索道停车场.jpg' },
-        { id: 11, name: '崇邑村', thumbnail: 'pic/photos/崇邑村.jpg', fullImage: 'pic/photos/崇邑村.jpg' },
-        { id: 12, name: '理想邦', thumbnail: 'pic/photos/理想邦.jpg', fullImage: 'pic/photos/理想邦.jpg' },
-        { id: 13, name: '理想邦不宰穷人', thumbnail: 'pic/photos/理想邦不宰穷人.jpg', fullImage: 'pic/photos/理想邦不宰穷人.jpg' },
-        { id: 14, name: '生态廊道', thumbnail: 'pic/photos/生态廊道.jpg', fullImage: 'pic/photos/生态廊道.jpg' },
-        { id: 15, name: '生态廊道小树林', thumbnail: 'pic/photos/生态廊道小树林.jpg', fullImage: 'pic/photos/生态廊道小树林.jpg' },
-        { id: 16, name: '风在圣托里尼', thumbnail: 'pic/photos/风在圣托里尼.jpg', fullImage: 'pic/photos/风在圣托里尼.jpg' },
-        { id: 17, name: '风口合照', thumbnail: 'pic/photos/风口合照.jpg', fullImage: 'pic/photos/风口合照.jpg' },
-        { id: 18, name: '小合照', thumbnail: 'pic/photos/小合照.jpg', fullImage: 'pic/photos/小合照.jpg' },
-        { id: 19, name: 'S湾', thumbnail: 'pic/photos/S湾.jpg', fullImage: 'pic/photos/S湾.jpg' },
-        { id: 20, name: 'S湾小B人', thumbnail: 'pic/photos/S湾小B人.jpg', fullImage: 'pic/photos/S湾小B人.jpg' },
-        { id: 21, name: '小咪小喵', thumbnail: 'pic/photos/小咪小喵.jpg', fullImage: 'pic/photos/小咪小喵.jpg' },
-        { id: 22, name: '风中的小咪小喵', thumbnail: 'pic/photos/风中的小咪小喵.jpg', fullImage: 'pic/photos/风中的小咪小喵.jpg' },
-        { id: 23, name: '悲伤的小咪小喵', thumbnail: 'pic/photos/悲伤的小咪小喵.jpg', fullImage: 'pic/photos/悲伤的小咪小喵.jpg' },
-        { id: 24, name: '韩男1', thumbnail: 'pic/photos/韩男1.jpg', fullImage: 'pic/photos/韩男1.jpg' },
-        { id: 25, name: '韩男2', thumbnail: 'pic/photos/韩男2.jpg', fullImage: 'pic/photos/韩男2.jpg' },
-        { id: 26, name: '韩男3', thumbnail: 'pic/photos/韩男3.jpg', fullImage: 'pic/photos/韩男3.jpg' },
-        { id: 27, name: '悲伤韩男', thumbnail: 'pic/photos/悲伤韩男.jpg', fullImage: 'pic/photos/悲伤韩男.jpg' },
-        { id: 28, name: '呆坐韩男', thumbnail: 'pic/photos/呆坐韩男.jpg', fullImage: 'pic/photos/呆坐韩男.jpg' },
-        { id: 29, name: '我们的摄影师', thumbnail: 'pic/photos/我们的摄影师.jpg', fullImage: 'pic/photos/我们的摄影师.jpg' },
-        { id: 30, name: '突兀的比心', thumbnail: 'pic/photos/突兀的比心.jpg', fullImage: 'pic/photos/突兀的比心.jpg' },
-        { id: 31, name: '冯哥说的指点江山', thumbnail: 'pic/photos/冯哥说的指点江山.jpg', fullImage: 'pic/photos/冯哥说的指点江山.jpg' },
-        { id: 32, name: '小牛在做甲马', thumbnail: 'pic/photos/小牛在做甲马.jpg', fullImage: 'pic/photos/小牛在做甲马.jpg' },
-        { id: 33, name: '蒋勇不记得的地方', thumbnail: 'pic/photos/蒋勇不记得的地方.jpg', fullImage: 'pic/photos/蒋勇不记得的地方.jpg' },
-        { id: 34, name: '最伟大的作品1', thumbnail: 'pic/photos/最伟大的作品1.jpg', fullImage: 'pic/photos/最伟大的作品1.jpg' },
-        { id: 35, name: '最伟大的作品2', thumbnail: 'pic/photos/最伟大的作品2.jpg', fullImage: 'pic/photos/最伟大的作品2.jpg' },
-        { id: 36, name: '最伟大的作品3', thumbnail: 'pic/photos/最伟大的作品3.jpg', fullImage: 'pic/photos/最伟大的作品3.jpg' },
-        { id: 37, name: '最伟大的作品4', thumbnail: 'pic/photos/最伟大的作品4.jpg', fullImage: 'pic/photos/最伟大的作品4.jpg' },
-        { id: 38, name: '风口合照', thumbnail: 'pic/photos/风口合照.jpg', fullImage: 'pic/photos/风口合照.jpg' }
+        { id: 1, name: '洱海海鸥', thumbnail: 'https://github.com/BUZI2022/dali2/releases/download/v1.0.0/photos.zip/洱海海鸥.jpg', fullImage: 'https://github.com/BUZI2022/dali2/releases/download/v1.0.0/photos.zip/洱海海鸥.jpg' },
+        { id: 2, name: '洱海孤树', thumbnail: 'https://github.com/BUZI2022/dali2/releases/download/v1.0.0/photos.zip/洱海孤树.jpg', fullImage: 'https://github.com/BUZI2022/dali2/releases/download/v1.0.0/photos.zip/洱海孤树.jpg' },
+        { id: 3, name: '洱海月', thumbnail: 'https://github.com/BUZI2022/dali2/releases/download/v1.0.0/photos.zip/洱海月.jpg', fullImage: 'https://github.com/BUZI2022/dali2/releases/download/v1.0.0/photos.zip/洱海月.jpg' },
+        { id: 4, name: '龙湾俯瞰', thumbnail: 'https://github.com/BUZI2022/dali2/releases/download/v1.0.0/photos.zip/龙湾俯瞰.jpg', fullImage: 'https://github.com/BUZI2022/dali2/releases/download/v1.0.0/photos.zip/龙湾俯瞰.jpg' },
+        { id: 5, name: '海东', thumbnail: 'https://github.com/BUZI2022/dali2/releases/download/v1.0.0/photos.zip/海东.jpg', fullImage: 'https://github.com/BUZI2022/dali2/releases/download/v1.0.0/photos.zip/海东.jpg' },
+        { id: 6, name: '海东滩涂', thumbnail: 'https://github.com/BUZI2022/dali2/releases/download/v1.0.0/photos.zip/海东滩涂.jpg', fullImage: 'https://github.com/BUZI2022/dali2/releases/download/v1.0.0/photos.zip/海东滩涂.jpg' },
+        { id: 7, name: '孤舟', thumbnail: 'https://github.com/BUZI2022/dali2/releases/download/v1.0.0/photos.zip/孤舟.jpg', fullImage: 'https://github.com/BUZI2022/dali2/releases/download/v1.0.0/photos.zip/孤舟.jpg' },
+        { id: 8, name: '凭栏小队', thumbnail: 'https://github.com/BUZI2022/dali2/releases/download/v1.0.0/photos.zip/凭栏小队.jpg', fullImage: 'https://github.com/BUZI2022/dali2/releases/download/v1.0.0/photos.zip/凭栏小队.jpg' },
+        { id: 9, name: '感通索道集市', thumbnail: 'https://github.com/BUZI2022/dali2/releases/download/v1.0.0/photos.zip/感通索道集市.jpg', fullImage: 'https://github.com/BUZI2022/dali2/releases/download/v1.0.0/photos.zip/感通索道集市.jpg' },
+        { id: 10, name: '感通索道停车场', thumbnail: 'https://github.com/BUZI2022/dali2/releases/download/v1.0.0/photos.zip/感通索道停车场.jpg', fullImage: 'https://github.com/BUZI2022/dali2/releases/download/v1.0.0/photos.zip/感通索道停车场.jpg' },
+        { id: 11, name: '崇邑村', thumbnail: 'https://github.com/BUZI2022/dali2/releases/download/v1.0.0/photos.zip/崇邑村.jpg', fullImage: 'https://github.com/BUZI2022/dali2/releases/download/v1.0.0/photos.zip/崇邑村.jpg' },
+        { id: 12, name: '理想邦', thumbnail: 'https://github.com/BUZI2022/dali2/releases/download/v1.0.0/photos.zip/理想邦.jpg', fullImage: 'https://github.com/BUZI2022/dali2/releases/download/v1.0.0/photos.zip/理想邦.jpg' },
+        { id: 13, name: '理想邦不宰穷人', thumbnail: 'https://github.com/BUZI2022/dali2/releases/download/v1.0.0/photos.zip/理想邦不宰穷人.jpg', fullImage: 'https://github.com/BUZI2022/dali2/releases/download/v1.0.0/photos.zip/理想邦不宰穷人.jpg' },
+        { id: 14, name: '生态廊道', thumbnail: 'https://github.com/BUZI2022/dali2/releases/download/v1.0.0/photos.zip/生态廊道.jpg', fullImage: 'https://github.com/BUZI2022/dali2/releases/download/v1.0.0/photos.zip/生态廊道.jpg' },
+        { id: 15, name: '生态廊道小树林', thumbnail: 'https://github.com/BUZI2022/dali2/releases/download/v1.0.0/photos.zip/生态廊道小树林.jpg', fullImage: 'https://github.com/BUZI2022/dali2/releases/download/v1.0.0/photos.zip/生态廊道小树林.jpg' },
+        { id: 16, name: '风在圣托里尼', thumbnail: 'https://github.com/BUZI2022/dali2/releases/download/v1.0.0/photos.zip/风在圣托里尼.jpg', fullImage: 'https://github.com/BUZI2022/dali2/releases/download/v1.0.0/photos.zip/风在圣托里尼.jpg' },
+        { id: 17, name: '风口合照', thumbnail: 'https://github.com/BUZI2022/dali2/releases/download/v1.0.0/photos.zip/风口合照.jpg', fullImage: 'https://github.com/BUZI2022/dali2/releases/download/v1.0.0/photos.zip/风口合照.jpg' },
+        { id: 18, name: '小合照', thumbnail: 'https://github.com/BUZI2022/dali2/releases/download/v1.0.0/photos.zip/小合照.jpg', fullImage: 'https://github.com/BUZI2022/dali2/releases/download/v1.0.0/photos.zip/小合照.jpg' },
+        { id: 19, name: 'S湾', thumbnail: 'https://github.com/BUZI2022/dali2/releases/download/v1.0.0/photos.zip/S湾.jpg', fullImage: 'https://github.com/BUZI2022/dali2/releases/download/v1.0.0/photos.zip/S湾.jpg' },
+        { id: 20, name: 'S湾小B人', thumbnail: 'https://github.com/BUZI2022/dali2/releases/download/v1.0.0/photos.zip/S湾小B人.jpg', fullImage: 'https://github.com/BUZI2022/dali2/releases/download/v1.0.0/photos.zip/S湾小B人.jpg' },
+        { id: 21, name: '小咪小喵', thumbnail: 'https://github.com/BUZI2022/dali2/releases/download/v1.0.0/photos.zip/小咪小喵.jpg', fullImage: 'https://github.com/BUZI2022/dali2/releases/download/v1.0.0/photos.zip/小咪小喵.jpg' },
+        { id: 22, name: '风中的小咪小喵', thumbnail: 'https://github.com/BUZI2022/dali2/releases/download/v1.0.0/photos.zip/风中的小咪小喵.jpg', fullImage: 'https://github.com/BUZI2022/dali2/releases/download/v1.0.0/photos.zip/风中的小咪小喵.jpg' },
+        { id: 23, name: '悲伤的小咪小喵', thumbnail: 'https://github.com/BUZI2022/dali2/releases/download/v1.0.0/photos.zip/悲伤的小咪小喵.jpg', fullImage: 'https://github.com/BUZI2022/dali2/releases/download/v1.0.0/photos.zip/悲伤的小咪小喵.jpg' },
+        { id: 24, name: '韩男1', thumbnail: 'https://github.com/BUZI2022/dali2/releases/download/v1.0.0/photos.zip/韩男1.jpg', fullImage: 'https://github.com/BUZI2022/dali2/releases/download/v1.0.0/photos.zip/韩男1.jpg' },
+        { id: 25, name: '韩男2', thumbnail: 'https://github.com/BUZI2022/dali2/releases/download/v1.0.0/photos.zip/韩男2.jpg', fullImage: 'https://github.com/BUZI2022/dali2/releases/download/v1.0.0/photos.zip/韩男2.jpg' },
+        { id: 26, name: '韩男3', thumbnail: 'https://github.com/BUZI2022/dali2/releases/download/v1.0.0/photos.zip/韩男3.jpg', fullImage: 'https://github.com/BUZI2022/dali2/releases/download/v1.0.0/photos.zip/韩男3.jpg' },
+        { id: 27, name: '悲伤韩男', thumbnail: 'https://github.com/BUZI2022/dali2/releases/download/v1.0.0/photos.zip/悲伤韩男.jpg', fullImage: 'https://github.com/BUZI2022/dali2/releases/download/v1.0.0/photos.zip/悲伤韩男.jpg' },
+        { id: 28, name: '呆坐韩男', thumbnail: 'https://github.com/BUZI2022/dali2/releases/download/v1.0.0/photos.zip/呆坐韩男.jpg', fullImage: 'https://github.com/BUZI2022/dali2/releases/download/v1.0.0/photos.zip/呆坐韩男.jpg' },
+        { id: 29, name: '我们的摄影师', thumbnail: 'https://github.com/BUZI2022/dali2/releases/download/v1.0.0/photos.zip/我们的摄影师.jpg', fullImage: 'https://github.com/BUZI2022/dali2/releases/download/v1.0.0/photos.zip/我们的摄影师.jpg' },
+        { id: 30, name: '突兀的比心', thumbnail: 'https://github.com/BUZI2022/dali2/releases/download/v1.0.0/photos.zip/突兀的比心.jpg', fullImage: 'https://github.com/BUZI2022/dali2/releases/download/v1.0.0/photos.zip/突兀的比心.jpg' },
+        { id: 31, name: '冯哥说的指点江山', thumbnail: 'https://github.com/BUZI2022/dali2/releases/download/v1.0.0/photos.zip/冯哥说的指点江山.jpg', fullImage: 'https://github.com/BUZI2022/dali2/releases/download/v1.0.0/photos.zip/冯哥说的指点江山.jpg' },
+        { id: 32, name: '小牛在做甲马', thumbnail: 'https://github.com/BUZI2022/dali2/releases/download/v1.0.0/photos.zip/小牛在做甲马.jpg', fullImage: 'https://github.com/BUZI2022/dali2/releases/download/v1.0.0/photos.zip/小牛在做甲马.jpg' },
+        { id: 33, name: '蒋勇不记得的地方', thumbnail: 'https://github.com/BUZI2022/dali2/releases/download/v1.0.0/photos.zip/蒋勇不记得的地方.jpg', fullImage: 'https://github.com/BUZI2022/dali2/releases/download/v1.0.0/photos.zip/蒋勇不记得的地方.jpg' },
+        { id: 34, name: '最伟大的作品1', thumbnail: 'https://github.com/BUZI2022/dali2/releases/download/v1.0.0/photos.zip/最伟大的作品1.jpg', fullImage: 'https://github.com/BUZI2022/dali2/releases/download/v2.0.0/photos.zip/最伟大的作品1.jpg' },
+        { id: 35, name: '最伟大的作品2', thumbnail: 'https://github.com/BUZI2022/dali2/releases/download/v1.0.0/photos.zip/最伟大的作品2.jpg', fullImage: 'https://github.com/BUZI2022/dali2/releases/download/v1.0.0/photos.zip/最伟大的作品2.jpg' },
+        { id: 36, name: '最伟大的作品3', thumbnail: 'https://github.com/BUZI2022/dali2/releases/download/v1.0.0/photos.zip/最伟大的作品3.jpg', fullImage: 'https://github.com/BUZI2022/dali2/releases/download/v1.0.0/photos.zip/最伟大的作品3.jpg' },
+        { id: 37, name: '最伟大的作品4', thumbnail: 'https://github.com/BUZI2022/dali2/releases/download/v1.0.0/photos.zip/最伟大的作品4.jpg', fullImage: 'https://github.com/BUZI2022/dali2/releases/download/v1.0.0/photos.zip/最伟大的作品4.jpg' },
+        { id: 38, name: '风口合照', thumbnail: 'https://github.com/BUZI2022/dali2/releases/download/v1.0.0/photos.zip/风口合照.jpg', fullImage: 'https://github.com/BUZI2022/dali2/releases/download/v1.0.0/photos.zip/风口合照.jpg' }
     ],
     locations: [
         {
             name: "大理古城",
             coordinates: [100.16, 25.69],
-            previewImage: 'pic/fuchuang/大理古镇.jpg'
+            previewImage: 'https://github.com/BUZI2022/dali2/releases/download/v1.0.0/fuchuang/大理古镇.jpg'
         },
         {
             name: "崇邑村",
             coordinates: [100.20, 25.64],
-            previewImage: 'pic/fuchuang/崇邑村.jpg'
+            previewImage: 'https://github.com/BUZI2022/dali2/releases/download/v1.0.0/fuchuang/崇邑村.jpg'
         },
         {
             name: "感通索道",
             coordinates: [100.10, 25.70],
-            previewImage: 'pic/fuchuang/感通索道.jpg'
+            previewImage: 'https://github.com/BUZI2022/dali2/releases/download/v1.0.0/fuchuang/感通索道.jpg'
         },
         {
             name: "理想邦",
             coordinates: [100.27, 25.70],
-            previewImage: 'pic/fuchuang/理想邦.jpg'
+            previewImage: 'https://github.com/BUZI2022/dali2/releases/download/v1.0.0/fuchuang/理想邦.jpg'
         },
         {
             name: "洱海龙湾",
             coordinates: [100.268, 25.615],
-            previewImage: 'pic/fuchuang/龙湾.jpg'
+            previewImage: 'https://github.com/BUZI2022/dali2/releases/download/v1.0.0/fuchuang/龙湾.jpg'
         }
     ]
 };
